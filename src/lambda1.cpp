59b73dfd098d57c4f9695b6af7e27f0bbd725487
root <jontromanab.abhijit@gmail.com>
2017-11-22 15:00:00 -0700

    lambda1

---
+++ src/lambda1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<functional>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+
+ //basic lambda function
+ auto sayHello = [](){cout<<"nope"<<endl;};
+ sayHello();
+
+ //lambda function with parameters
+ return 0;
+}
