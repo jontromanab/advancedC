6c8ce76dfb5ed111609e842c4179c6fa3a00ca6b
root <jontromanab.abhijit@gmail.com>
2017-11-22 15:04:57 -0700

    lambda1

--- src/lambda1.cpp
+++ src/lambda1.cpp
@@ -12,5 +12,14 @@ int main(){
  sayHello();
 
  //lambda function with parameters
+ auto sayHelloP = [](int a, int b){return a+b;};
+ cout<<"SayHello says: "<<sayHelloP(190, 10)<<endl;
+
+ //lambda function with parameters optional argument
+ auto product =[](int x, int y) -> int //it is something like a function pointer
+ {
+   return x*y;
+ };
+ cout<<"Product is: "<<product(7,10)<<endl;
  return 0;
 }
