402a2f433abdb54c36aa5c3610bc754cb20938d5
jontromanab.abhijit@gmail.com <jontromanab.abhijit@gmail.com>
2017-11-24 23:44:36 -0700

    boost_maro2

--- src/1_boost_macro.cpp
+++ src/1_boost_macro.cpp
@@ -7,12 +7,24 @@
 
 #define MY_NUM 1300
 
-void testMacros(){
+void testMacroA(){
   std::cout<<"You have called: "<<BOOST_CURRENT_FUNCTION<<std::endl;
+}
+
+void testMacroB(){
+  BOOST_STATIC_ASSERT(MY_NUM!=1400);
+}
+
+void testMacroC(){
+  std::vector<double> myVec(10);
+  BOOST_FOREACH(double& x, myVec) x =10.0;
+  BOOST_FOREACH(double x, myVec) std::cout<<x<<std::endl;
 }
 
 int main()
 {
-  testMacros();
+  testMacroA();
+  testMacroB();
+  testMacroC();
   return 0;
 }
