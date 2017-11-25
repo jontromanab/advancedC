7428240318a21c1c90beae5d4f2cd1be05f5867f
jontromanab.abhijit@gmail.com <jontromanab.abhijit@gmail.com>
2017-11-24 23:52:06 -0700

    boost_maro3

--- src/1_boost_macro.cpp
+++ src/1_boost_macro.cpp
@@ -7,18 +7,30 @@
 
 #define MY_NUM 1300
 
+//Returns current function name
 void testMacroA(){
   std::cout<<"You have called: "<<BOOST_CURRENT_FUNCTION<<std::endl;
 }
 
+// generates a compile time error message, If the input expression is false. If it is 1300, then there will be error
 void testMacroB(){
   BOOST_STATIC_ASSERT(MY_NUM!=1400);
 }
 
+//iteration by boost. first iteration writes, and second iteration reads
 void testMacroC(){
   std::vector<double> myVec(10);
   BOOST_FOREACH(double& x, myVec) x =10.0;
   BOOST_FOREACH(double x, myVec) std::cout<<x<<std::endl;
+}
+
+
+//part of lightweight test header. boost_error prints an error message with the name and location of error
+// boost_test tests if supplied condition is true and prints and error otherwise
+void testMacroD(){
+  BOOST_ERROR("Failure of this function");
+  double x = 0.0;
+  BOOST_TEST(x!=0);
 }
 
 int main()
@@ -26,5 +38,6 @@ int main()
   testMacroA();
   testMacroB();
   testMacroC();
+  testMacroD();
   return 0;
 }
