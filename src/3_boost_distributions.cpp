732ed222cec083e5c9949208557cc0b636afef31
jontromanab.abhijit@gmail.com <jontromanab.abhijit@gmail.com>
2017-11-26 23:53:28 -0700

    boost_distribution_1

---
+++ src/3_boost_distributions.cpp
@@ -0,0 +1,26 @@
+/*Ther are several kind of distribtions present in Boost
+
+1. Bernoulli Distribution
+2. Beta Distribution
+3. Binomial Distribution
+4. Cauchy-Lorentz Distribution
+5. Chi Squared Distribution
+6. Exponential Distribution
+7. Extreme Value Distribution
+8. F Distribution
+9. Gamma and Erlang Distribution
+10. Log Normal Distribution
+11. Negative Binomial Distribution
+12. Noncentral Beta Distribution
+13. Noncetral Chi-Squared Distribution
+14. Noncentral F Distribution
+15. Noncentral T Distribution
+16. Nornal (gaussian) Distribution
+17. Pareto Distribution
+18. Poisson Distribution
+19. Rayleigh Distribution
+20. Students T Distribution
+21. Triangular Distribution
+22. Weibull Distribution
+23. Uniform Distribution
+*/
