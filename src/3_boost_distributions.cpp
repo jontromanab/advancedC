80bc1cd92c24e618125decb082f22d6b633682d1
jontromanab.abhijit@gmail.com <jontromanab.abhijit@gmail.com>
2017-11-27 00:05:21 -0700

    boost_distribution_3

--- src/3_boost_distributions.cpp
+++ src/3_boost_distributions.cpp
@@ -41,3 +41,30 @@ excess kurtosis: kurtosis_excess(distribution)
 
 */
 
+#include <iostream>
+#include <boost/math/distributions.hpp>
+
+void distributionFn(){
+  boost::math::normal_distribution<> d(0.5,1);
+
+  std::cout<<"CDF: "<<cdf(d, 0.2)<<std::endl;
+  std::cout<<"PDF: "<<pdf(d, 0.0)<<std::endl;
+  std::cout<<"Inverse: "<<quantile(d, 0.1)<<std::endl;
+  std::cout<<"Mode: "<<mode(d)<<std::endl;
+  std::cout<<"Variance: "<<variance(d)<<std::endl;
+  std::cout<<"SD: "<<standard_deviation(d)<<std::endl;
+  std::cout<<"Skew: "<<skewness(d)<<std::endl;
+  std::cout<<"Kurtosis: "<<kurtosis(d)<<std::endl;
+  std::cout<<"Excess Kurt: "<<kurtosis_excess(d)<<std::endl;
+
+  std::pair<double, double> sup = support(d);
+  std::cout<<"Left sup: "<<sup.first<<std::endl;
+  std::cout<<"Right sup: "<<sup.second<<std::endl;
+}
+
+int main(){
+  distributionFn();
+  return 0;
+}
+
+
