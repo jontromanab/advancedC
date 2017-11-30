63823af5f0a011c6a54d007219865f80a9120b53
jontromanab.abhijit@gmail.com <jontromanab.abhijit@gmail.com>
2017-11-29 20:29:37 -0700

    boost_random_number2

--- src/4_boost_random_number.cpp
+++ src/4_boost_random_number.cpp
@@ -27,3 +27,46 @@ cauchy_distribution
 normal_distribution
 lognormal_distribution
 */
+
+#include<iostream>
+#include<boost/random.hpp>
+
+
+//generate discrete uniform random numbers in the set {1....6} with Mersenne-Twister
+void randomFunc1(){
+  //create seed
+  unsigned long seed = 12411;
+  //produces general pseudo random number series with the Mersenne Twister algo
+  boost::mt19937 rng(seed);
+  //uniform distribution in 1....6
+  boost::uniform_int<> six(1,6);
+  //connects distribution with random series
+  boost::variate_generator<boost::mt19937&, boost::uniform_int<> > unsix(rng, six);
+
+  std::cout<<unsix()<<std::endl;
+  std::cout<<unsix()<<std::endl;
+  std::cout<<unsix()<<std::endl;
+}
+
+////generate random number by Fibonacci generator
+void randomFunc2(){
+  //create seed
+  unsigned long seed = 89210;
+  //produces general pseudo random number series with lagged fibonacci algo
+  boost::lagged_fibonacci1279 rng(seed);
+  //normal distribution with mean 10 and st deviation 0.1
+  boost::normal_distribution<> norm(10,0.1);
+  //connects distribution with random series
+  boost::variate_generator<boost::lagged_fibonacci1279&, boost::normal_distribution<> > unnorm(rng, norm);
+  std::cout<<unnorm()<<std::endl;
+  std::cout<<unnorm()<<std::endl;
+  std::cout<<unnorm()<<std::endl;
+}
+
+
+int main(){
+
+  randomFunc1();
+  randomFunc2();
+  return 0;
+}
