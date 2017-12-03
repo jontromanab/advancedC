c4fb82b6f719b45820ba38ee48f2eb45df953bbc
jontromanab.abhijit@gmail.com <jontromanab.abhijit@gmail.com>
2017-12-02 21:13:05 -0700

    boost_any_2

--- src/7_boost_any.cpp
+++ src/7_boost_any.cpp
@@ -24,7 +24,33 @@ void callAny(boost::any x){
   //Do nothing
 }
 
+/*
+The type of any variable can be checked with the type() function against the standard typeid(T) function.
+To retrieve the original variable back, use the any_Cast function
+
+T* ptrMyT = boost::any_cast<T>(&myAny);
+
+*/
+
+void AnyTesting2(){
+  boost::any myAny;
+  double myDbl(1.1);
+  myAny = myDbl;
+  bool isDbl = myAny.type() == typeid(double);
+  std::cout<<"Is double: "<<isDbl<<std::endl;
+  bool isString = myAny.type() ==typeid(std::string);
+  std::cout<<"Is String: "<<isString<<std::endl;
+
+  double* ptrMyDbl = boost::any_cast<double>(&myAny);
+  if(ptrMyDbl!=NULL) std::cout<<"My double: "<<*ptrMyDbl<<std::endl;
+
+  int* ptrMyInt = boost::any_cast<int>(&myAny);
+  if(ptrMyInt == NULL) std::cout<<"Cast failed"<<std::endl;
+}
+
+
 int main(){
   AnyTesting1();
+  AnyTesting2();
   return 0 ;
 }
