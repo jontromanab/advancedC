f721c77b092c16b2311423ba0cabc3f8a3936cc8
root <jontromanab.abhijit@gmail.com>
2017-12-07 12:50:28 -0700

    boost_filesystem_2

--- src/10_boost_filesystem.cpp
+++ src/10_boost_filesystem.cpp
@@ -20,7 +20,27 @@ void testingFileSystem1(){
   std::cout<<pathExists<<std::endl;
 }
 
+void testingFileSystem2(){
+  fs::path myPath("/home/abhi/advancedC/");
+  fs::directory_iterator itr(myPath);
+  fs::directory_iterator end_itr;
+
+  std::cout<<myPath.root_path()<<std::endl;
+  std::cout<<myPath.parent_path()<<std::endl;
+
+  while(itr!=end_itr && !fs::is_directory(itr->path())){
+    std::cout<<"---------------------------------------------------"<<std::endl;
+    std::cout<<"Path: "<<itr->path()<<std::endl;
+    std::cout<<"Filename: "<<itr->path().filename()<<std::endl;
+    std::cout<<"Is file: "<<fs::is_regular_file(itr->path())<<std::endl;
+    std::cout<<"File Size :"<<fs::file_size(itr->path())<<std::endl;
+    std::cout<<"----------------------------------------------------"<<std::endl;
+    ++itr;
+  }
+}
+
 int main(){
   testingFileSystem1();
+  testingFileSystem2();
   return 0;
 }
