4d27a484101a74bfe2b2f93f1e397920f62d2566
jontromanab.abhijit@gmail.com <jontromanab.abhijit@gmail.com>
2017-12-13 03:34:34 -0700

    virtual f in constructor

---
+++ src/virtual_function_in_constructor.cpp
@@ -0,0 +1,26 @@
+//Calling virtual function in constructor or destructor
+
+#include<iostream>
+#include<memory>
+using namespace std;
+
+class Dog{
+public:
+  Dog() {cout<<"Dog born."<<endl;}//bark().. We dont need this. calling main class functions before base class's constructor}
+  virtual void bark(){cout<<"I am just a dog."<<endl;}
+  void seeCat(){bark();}
+};
+
+class YellowDog : public Dog{
+public:
+  YellowDog(){cout<<"Yellow dog is born"<<endl;}
+  virtual void bark(){cout<<"I am a yellow dog"<<endl;}
+
+};
+
+
+int main(){
+  YellowDog d;
+  d.seeCat();
+  return 0;
+}
